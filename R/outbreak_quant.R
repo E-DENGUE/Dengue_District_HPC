5c5c06363db504bdf370aa4d6ba5ac26c5b3b646
weinbergerlab <dweinber@gmail.com>
2024-03-08 12:47:44 -0500

    clean up code

--- R/outbreak_quant.R
+++ R/outbreak_quant.R
@@ -5,6 +5,7 @@ library(zoo)
 library(ggplot2)
 library(exactci)
 library(RcppRoll)
+library(patchwork)
 d2 <- readRDS('./Data/CONFIDENTIAL/full_data_with_new_boundaries_all_factors_cleaned.rds') 
   
 all.districts <- unique(d2$district) 
@@ -186,15 +187,15 @@ mean(ds2_pois$epidemic_flag_fixed, na.rm=T)
 ## quantify performance of the different cutoffs. We want to have high values for:
 ### what proportion of cases in each year occur after the epidemic is observed?
 ### what is the incidence of cases that occur each year after the epidemic is declared
-##This creates a 6 month moving window for each district to see if an alarm has been triggered in last 6 months (0=no alarm)
+##This creates a 4 month moving window for each district to see if an alarm has been triggered in last 4 months (0=no alarm)
 e1 <- ds2_pois %>%
   arrange(district, date) %>%
   mutate(year=year(date)) %>%
   group_by(district) %>%
-  mutate(alarmN= RcppRoll::roll_sum(epidemic_flag, n=6,align = "right", fill = NA,partial = FALSE),
-         alarmN_pois=RcppRoll::roll_sum(epidemic_flag_poisson, n=6,align = "right", fill = NA,partial = FALSE),
-         alarmN_quant=RcppRoll::roll_sum(epidemic_flag_quant, n=6,align = "right", fill = NA,partial = FALSE),
-         alarmN_fixed=RcppRoll::roll_sum(epidemic_flag_fixed), n=6,align = "right", fill = NA,partial = FALSE)%>%
+  mutate(alarmN= RcppRoll::roll_sum(epidemic_flag, n=4,align = "right", fill = NA,partial = FALSE),
+         alarmN_pois=RcppRoll::roll_sum(epidemic_flag_poisson, n=4,align = "right", fill = NA,partial = FALSE),
+         alarmN_quant=RcppRoll::roll_sum(epidemic_flag_quant, n=4,align = "right", fill = NA,partial = FALSE),
+         alarmN_fixed=RcppRoll::roll_sum(epidemic_flag_fixed), n=4,align = "right", fill = NA,partial = FALSE)%>%
   ungroup()%>%
   mutate(N_epidemic_sd = if_else(alarmN>0,m_DHF_cases,NA_real_) ,
          N_epidemic_pois = if_else(alarmN_pois>0,m_DHF_cases,NA_real_) ,
@@ -220,22 +221,32 @@ e1 <- ds2_pois %>%
             inc_epidemic_pois = N_epidemic_pois/pop*100000,
             inc_epidemic_fixed = N_epidemic_fixed/pop*100000,
             )
-
+saveRDS(e1, './Data/observed_alarms.rds')
 ##What we want is stuff in upper quadrant: large proportion of cases occur after epidemic is declared and incidence is high after epidemic is declared.
-ggplot(e1, aes(x=inc_epidemic_2sd, y=prop_epidemic_2sd))+
-  geom_point()+
-  ylim(0,1)
+p1<-ggplot(e1, aes(x=inc_epidemic_2sd, y=prop_epidemic_2sd))+
+  geom_point(alpha=0.2)+
+  ylim(0,1)+
+  ggtitle('Mean+2SD')+
+  theme_classic()
 
-ggplot(e1, aes(x=inc_epidemic_quant, y=prop_epidemic_quant))+
-  geom_point()+
-  ylim(0,1)
+p2<- ggplot(e1, aes(x=inc_epidemic_quant, y=prop_epidemic_quant))+
+  geom_point(alpha=0.2)+
+  ylim(0,1)+
+  ggtitle('Quantile (95%tile)')+
+  theme_classic()
 
-ggplot(e1, aes(x=inc_epidemic_pois, y=prop_epidemic_pois))+
-  geom_point()+
-  ylim(0,1)
+p3 <- ggplot(e1, aes(x=inc_epidemic_pois, y=prop_epidemic_pois))+
+  geom_point(alpha=0.2)+
+  ylim(0,1)+
+  ggtitle('Poisson prediction interval')+
+  theme_classic()
 
-ggplot(e1, aes(x=inc_epidemic_fixed, y=prop_epidemic_fixed))+
-  geom_point()+
-  ylim(0,1)
 
+p4<- ggplot(e1, aes(x=inc_epidemic_fixed, y=prop_epidemic_fixed))+
+  geom_point(alpha=0.2)+
+  ylim(0,1)+
+ggtitle('Fixed threshold')+
+  theme_classic()
+
+(p1+p2)/(p3+p4)
   
