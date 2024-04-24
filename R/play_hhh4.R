3f87c22c42974d7b289b1c2e2462777f99244856
weinbergerlab <dweinber@gmail.com>
2024-04-23 22:52:31 -0400

    hhh4

---
+++ R/play_hhh4.R
@@ -0,0 +1,139 @@
+library(surveillance)
+library(sf)
+library(dplyr)
+library(lubridate)
+library(fanplot)
+MDR_NEW <- readRDS( "./Data/MDR_NEW.rds") %>%
+  arrange(ID)
+
+row.names(MDR_NEW) <- MDR_NEW$VARNAME
+
+neighb <- poly2adjmat(st_make_valid(MDR_NEW))
+
+dist_nbOrder <- nbOrder(neighb)
+
+colnames(dist_nbOrder) <- MDR_NEW$VARNAME
+
+map1 <- sf:::as_Spatial(MDR_NEW)
+
+#rowSums(neighb)
+d2 <- readRDS('./Data/CONFIDENTIAL/full_data_with_new_boundaries_all_factors_cleaned.rds') %>%
+  rename(District_province = prov_dis) %>%
+  filter(date<= "2012-12-01")
+
+start.date <- '2004-07-01'
+start.year <- year(start.date)
+start.week <- week(start.date)
+start.month <- month(start.date)
+
+
+cases <- d2 %>% 
+          reshape2::dcast(date~district, value.var= 'm_DHF_cases') %>%
+  filter(date>=start.date) %>%
+  dplyr::select(unique(MDR_NEW$VARNAME))%>%
+  as.matrix()
+
+pop <- d2 %>% 
+  mutate(pop2=pop/100000) %>%
+  reshape2::dcast(date~district, value.var= 'pop2') %>%
+  filter(date>=start.date) %>%
+  dplyr::select(unique(MDR_NEW$VARNAME))%>%
+  as.matrix()
+
+unique(MDR_NEW$VARNAME) == colnames(pop)
+
+#Define STS object
+dengue_df <- sts(cases, start = c(start.year, start.month), frequency = 12,
+                        population = pop, neighbourhood = dist_nbOrder, map=map1)
+
+plot(dengue_df, type = observed ~ time)
+
+plot(dengue_df, type = observed ~ unit,
+#      population = dengue_df@map$POPULATION / 100000,
+      labels = list(font = 2), colorkey = list(space = "right"),
+      sp.layout = layout.scalebar(dengue_df@map, corner = c(0.05, 0.05),
+                                    scale = 50, labels = c("0", "50 km"), height = 0.03))
+
+dengue_mod_basic <- list(
+   end = list(f = addSeason2formula(~1 + t, period = dengue_df@freq),
+                offset = population(dengue_df)),
+   ar = list(f = ~1),
+   ne = list(f = ~1, weights = neighbourhood(dengue_df) == 1),
+   family = "NegBin1")
+
+dengueFit_basic <- hhh4(stsObj = dengue_df, control = dengue_mod_basic)
+
+summary(dengueFit_basic, idx2Exp = TRUE, amplitudeShift = TRUE, maxEV = TRUE)
+
+#decomposition of the fitted model ; orange=spatiotempiral, gray=endemic, blue=autoregressive
+districts2plot <- which(colSums(observed(dengue_df)) > 50)
+ par(mfrow = c(2,3), mar = c(3, 5, 2, 1), las = 1)
+ plot(dengueFit_basic, type = "fitted", units = districts2plot,
+         hide0s = TRUE, par.settings = NULL, legend = 1)
+ 
+ #overall
+ plot(dengueFit_basic, type = "fitted", total = TRUE,
+        hide0s = TRUE, par.settings = NULL, legend = FALSE) -> fitted_components
+
+ 
+ #Update model, by AIC is worse than basic model
+ dengueFit_nepop <- update(dengueFit_basic,
+                             ne = list(f = ~log(pop)), data = list(pop = population(dengue_df)))
+
+ #Power law weights; this improves a lot
+ dengueFit_nepop_powerlaw <- update(dengueFit_nepop,
+                                ne = list(weights = W_powerlaw(maxlag = 5)))
+ 
+ #unconstrianed weights
+ dengueFit_np2 <- update(dengueFit_nepop,
+                           ne = list(weights = W_np(maxlag = 2)))
+
+ AIC(dengueFit_basic)
+ AIC(dengueFit_nepop)
+ AIC(dengueFit_nepop_powerlaw)
+ AIC(dengueFit_np2) #winner
+ 
+ #add random intercept
+ dengueFit_ri <- update(dengueFit_np2,
+                          end = list(f = update(formula(dengueFit_np2)$end, ~. + ri() - 1)),
+                          ar = list(f = update(formula(dengueFit_np2)$ar, ~. + ri() - 1)),
+                          ne = list(f = update(formula(dengueFit_np2)$ne, ~. + ri() - 1)))
+  summary(dengueFit_ri)
+ par(mfrow = c(2,3), mar = c(3, 5, 2, 1), las = 1)
+  plot(dengueFit_ri, type = "fitted", units = districts2plot,
+          hide0s = TRUE, par.settings = NULL, legend = 1)
+  plot(dengueFit_ri, type = "fitted", total = TRUE,
+          hide0s = TRUE, par.settings = NULL, legend = FALSE)
+ 
+  
+  models2compare <- c("dengueFit_np2", "dengueFit_ri", "dengueFit_basic")
+  tp = c(80,100)
+  
+  #This takes some time when doing rolling; RI model is best in both instances
+  dengue_preds1 <- lapply(mget(models2compare), oneStepAhead,    tp = tp, type = "rolling")
+  
+  SCORES <- c("logs", "rps", "dss", "ses")
+  dengueScores1 <- lapply(dengue_preds1, scores, which = SCORES, individual = TRUE)
+  t(sapply(dengueScores1, colMeans, dims = 2))  
+  
+  #Forward simulation
+  dengue_mod_ri <- list(
+    end = list(f = addSeason2formula(~0 + t + ri() , period = dengue_df@freq),
+               offset = population(dengue_df)),
+    ar = list(f = ~0 + ri() ),
+    ne = list(f = ~0 + ri() - 1, weights = W_np(maxlag = 2)),
+    family = "NegBin1",
+    subset = 2:100
+    )
+  
+  dengueFit_ri <- hhh4(stsObj = dengue_df, control = dengue_mod_ri)
+  
+    dengueSim <- simulate(dengueFit_ri,
+                          nsim = 99, seed = 1, subset = 101:102)
+
+  par(mfrow = c(1,1), mar = c(3, 5, 2, 1), las = 1)
+  
+  plot(dengueFit_ri, type = "fitted", total = TRUE,
+       hide0s = TRUE, par.settings = NULL, legend = FALSE)
+  plot(dengueSim, "fan", means.args = list(), key.args = list(), add=T)
+  
