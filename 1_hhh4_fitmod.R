460d7a27d610443a6f332f97743325d319b499c0
weinbergerlab <dweinber@gmail.com>
2024-04-26 09:15:20 -0400

    hhh4 add models

--- 1_hhh4_fitmod.R
+++ 1_hhh4_fitmod.R
@@ -27,8 +27,8 @@ args <- commandArgs(trailingOnly = TRUE)
 j <- as.numeric(args[1])  
 k <- as.numeric(args[2])
 
-# mods <- c('hhh4_np','hhh4_power','hhh4_basic','hhh4_power_precip_temp')
-mods <- c('hhh4_power_precip_temp')
+ mods <- c('hhh4_np','hhh4_power','hhh4_basic','hhh4_power_precip_temp','hhh4_power_precip_temp_endmc1','hhh4_power_precip_temp_endmc2')
+#mods <- c('hhh4_power_precip_temp')
 
 #i=1 #108 dates total
 #k=1 #10 models
--- 1_hhh4_fitmod.sh
+++ 1_hhh4_fitmod.sh
@@ -7,12 +7,12 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1G
 #SBATCH -o ./Report/output.%a.out # STDOUT
-#SBATCH --array=1-252   # If k models and J hold out time points this is 1- j*k  J=84, K=15
+#SBATCH --array=1-504   # If k models and J hold out time points this is 1- j*k  J=84, K=15
 
 
 #Define the number of models being tested
 
-N_models=3
+N_models=6
 
 #Load R
 module load R/4.2.0-foss-2020b
