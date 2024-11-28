075226fc3ac8419d94b2716ef60cf99727f6e8f7
Weinberger <daniel.weinberger@yale.edu>
2024-11-27 22:58:42 -0500

    improve viz

--- R/threshold_app/app3.R
+++ R/threshold_app/app3.R
@@ -4,25 +4,9 @@ library(ggplot2)
 
 # Define UI for the app
 ui <- fluidPage(
-  titlePanel("Interactive Model Adjustment"),
+  titlePanel("Risk profiles and thresholds"),
   
-  div(
-    style = "margin-bottom: 20px; padding: 10px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 5px;",
-    p("This app demonstrates how the characteristics of forecasts of cases counts
-      affects risk scores. We simulate data from a mixture of a log-normal/Poisson distribution for one data point, which is a mixture of two underlying distributions governed by parameters lambda1 and lambda2 and sd1 and sd2.
-      We can control
-      the mean number of cases (lambda) and the amount of dispersion in the data (SD). We can also control
-      how far above a historical baselines the mean of these forecasts are (RR). The historgram on the
-      left shows the posterior distribution of the forecasts for the two populations. The dotted line shows the mean+2SD of
-      the historic baseline (97.5th percentile of the historic).
-      The plot on the right shows the risk profile, which is P(X>A)*A where A are different
-      threshold values. The maximum of these profiles gives the risk score.
-      Separately, we can calculate the probability that the forecast is greater than the
-      historical distribution, and we can create a combined risk score by multiplying that
-      probability by the risk score estimated from the profile")
-  ),
-  
-  sidebarLayout(
+   sidebarLayout(
     sidebarPanel(
       sliderInput("lambda_historic", "Lambda Historic:", min = 10, max = 500, value = 60, step = 10),
       sliderInput("RR1_in", "RR forecast Mod1:", min = 1, max = 2, value = 1.5, step = 0.1),
@@ -38,6 +22,21 @@ ui <- fluidPage(
       verbatimTextOutput("probability_output"),
       plotOutput("risk_plots")
     )
+  ),
+  div(
+    style = "margin-bottom: 20px; padding: 10px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 5px;",
+    p("This app demonstrates how the characteristics of forecasts of cases counts
+      affects risk scores. We simulate data from a mixture of a log-normal/Poisson distribution for one data point, which is a mixture of two underlying distributions governed by parameters lambda1 and lambda2 and sd1 and sd2.
+      We can control
+      the mean number of cases (lambda) and the amount of dispersion in the data (SD). We can also control
+      how far above a historical baselines the mean of these forecasts are (RR). The historgram on the
+      left shows the posterior distribution of the forecasts for the two populations. The dotted line shows the mean+2SD of
+      the historic baseline (97.5th percentile of the historic).
+      The plot on the right shows the risk profile, which is P(X>A)*A where A are different
+      threshold values. The maximum of these profiles gives the risk score.
+      Separately, we can calculate the probability that the forecast is greater than the
+      historical distribution, and we can create a combined risk score by multiplying that
+      probability by the risk score estimated from the profile")
   )
 )
 
@@ -69,6 +68,9 @@ server <- function(input, output) {
     
     b1 <- bind_rows(b1a, b1b)
     
+    ucl = quantile(b1$historic1,probs=0.975)
+    historic_mean = mean(b1$historic1, na.rm=T)
+    
     prob_RR1_gt_1 <- mean(b1$RR1 > 1)
     
     historic1_mean = mean(b1$historic1, na.rm=T)
@@ -86,6 +88,14 @@ server <- function(input, output) {
       dplyr::select(predN, probability1)
     
     pop_score_compare <- pop1_scores 
+    
+    probs_high_risk <- pop1_scores %>%
+      filter(predN>ucl) %>%
+      summarize(probs_high_risk=sum(probability1))
+    
+    probs_med_risk <- pop1_scores %>%
+      filter(predN>historic_mean & predN<=ucl) %>%
+      summarize(probs_med_risk=sum(probability1))
     
     thresholds <- 1:max(pop_score_compare$predN,na.rm=T)
     
@@ -109,6 +119,8 @@ server <- function(input, output) {
       risk.threshold1 = risk.threshold1,
       risk.threshold1z = risk.threshold1z,
       thresholds=thresholds,
+      probs_high_risk=probs_high_risk,
+      probs_med_risk=probs_med_risk,
       b1=b1
     )
   })
@@ -116,15 +128,19 @@ server <- function(input, output) {
   # Output probabilities
   output$probability_output <- renderPrint({
     scores <- calc_scores()
-    cat("Max Score Pop 1:", max(scores$risk.threshold1, na.rm=T) ,"\n")
+    cat("Max Score:", max(scores$risk.threshold1, na.rm=T) ,"\n")
     
     "\n"
-    cat("Prob greater than baseline Pop1:", scores$prob_RR1_gt_1, "\n")
+    cat("Prob greater than baseline:", scores$prob_RR1_gt_1, "\n")
     
     "\n"
-    cat("Combined score Pop1:", scores$prob_RR1_gt_1*max(scores$risk.threshold1, na.rm=T), "\n")
+    cat("Combined score:", scores$prob_RR1_gt_1*max(scores$risk.threshold1, na.rm=T), "\n")
     
-    cat("Max Score (Z) Pop 1:", max(scores$risk.threshold1z, na.rm=T) ,"\n")
+    cat("Max Score (Z):", max(scores$risk.threshold1z, na.rm=T) ,"\n")
+
+    cat("P(Mean->Mean+2SD):", scores$probs_med_risk$probs_med_risk ,"\n")
+    
+    cat("P(>Mean+2sd):", scores$probs_high_risk$probs_high_risk ,"\n")
     
   })
   
@@ -148,19 +164,44 @@ server <- function(input, output) {
     
     riskdf <- cbind.data.frame(max.risk.index,max.risk)
     riskdfz <- cbind.data.frame(max.risk.index.z,max.risk.z)
+    lims1=cbind.data.frame(historic_mean,ucl)
     
     p1 <- ggplot(pop_score_compare) +
+      geom_rect(data=lims1,aes(xmin=0,
+                               xmax = historic_mean,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#31a35420')+
+      geom_rect(data=lims1,aes(xmin=historic_mean,
+                               xmax = ucl,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#fec44f20')+
+      geom_rect(data=lims1,aes(xmin=ucl,
+                               xmax = Inf,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#d7191c20')+
       geom_vline(xintercept=ucl, lty=3, col='black', alpha=0.5)+
       geom_vline(xintercept=historic_mean, lty=3, col='black', alpha=0.5)+
-      geom_line(aes(x = thresholds, y = risk.threshold1), color = "#e41a1c") +
-      geom_point(data=riskdf,aes(x=max.risk.index, y=max.risk), col='red')+
+      geom_line(aes(x = thresholds, y = risk.threshold1), color = "#2b83ba") +
       xlim(0,max(pop_score_compare$thresholds, na.rm=T))+
       theme_classic() +
+      geom_point(data=riskdfz,aes(x=max.risk.index, y=max.risk), col='#2b83ba')+
       ggtitle("Risk profile")
     
     p2 <- ggplot(scores$b1) +
-      geom_histogram(aes(x=forecast1),binwidth=5, col='#e41a1c',fill='#e41a1c', alpha=0.5)+
-      geom_histogram(aes(x=historic1),binwidth=5, col='gray', alpha=0.1)+
+      geom_rect(data=lims1,aes(xmin=0,
+                               xmax = historic_mean,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#31a35420')+
+      geom_rect(data=lims1,aes(xmin=historic_mean,
+                               xmax = ucl,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#fec44f20')+
+      geom_rect(data=lims1,aes(xmin=ucl,
+                               xmax = Inf,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#d7191c20')+
+      geom_histogram(aes(x=historic1),binwidth=5, col='gray')+
+      geom_histogram(aes(x=forecast1),binwidth=5, col='#2b83ba',fill='#2b83ba', alpha=0.5)+
       geom_vline(xintercept=ucl, lty=3, col='black', alpha=0.5)+
       geom_vline(xintercept=historic_mean, lty=3, col='black', alpha=0.5)+
       theme_classic()+
@@ -168,13 +209,25 @@ server <- function(input, output) {
       xlim(0,max(pop_score_compare$thresholds, na.rm=T))
     
     p3 <- ggplot(pop_score_compare) +
-      geom_line(aes(x = thresholds, y = risk.threshold1z), color = "#e41a1c") +
+      geom_rect(data=lims1,aes(xmin=0,
+                               xmax = historic_mean,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#31a35420')+
+      geom_rect(data=lims1,aes(xmin=historic_mean,
+                               xmax = ucl,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#fec44f20')+
+      geom_rect(data=lims1,aes(xmin=ucl,
+                               xmax = Inf,
+                               ymin = -Inf,
+                               ymax = Inf), fill = '#d7191c20')+
+      geom_line(aes(x = thresholds, y = risk.threshold1z), color = "#2b83ba") +
       theme_classic() +
       geom_vline(xintercept=ucl, lty=3, col='black', alpha=0.5)+
       geom_vline(xintercept=historic_mean, lty=3, col='black', alpha=0.5)+
       xlim(0,max(pop_score_compare$thresholds, na.rm=T))+
       ggtitle("Risk profile (Z-score)")+
-      geom_point(data=riskdfz,aes(x=max.risk.index.z, y=max.risk.z), col='red')
+      geom_point(data=riskdfz,aes(x=max.risk.index.z, y=max.risk.z), col='#2b83ba')
     
     
     gridExtra::grid.arrange( p2,p3,p1, ncol=1)
