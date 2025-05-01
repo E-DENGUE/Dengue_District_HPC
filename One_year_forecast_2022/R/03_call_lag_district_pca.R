9570114df7a1fd1dc7571f447b3894ed12edc143
waladraidi <wala.draidi@hotmail.com>
2025-05-01 11:13:32 +1000

    Add Dengue_after_2022_informative_prior scripts and data

---
+++ One_year_forecast_2022/R/03_call_lag_district_pca.R
@@ -0,0 +1,13 @@
+source('./R/99_load.R')
+
+# Parse command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+j <- as.numeric(args[1])  
+k <- as.numeric(args[2])
+i <- as.numeric(args[3])
+
+mods <- c('PC_lags_weather','PC_lags','PC_weather','PC_without_seasonality','PC_without_seasonality_and_weather')
+
+
+
+mod1 <- lag_district_pca(vintage_date = date.test2[j], district.select=all.districts[k],modN=i ) 
